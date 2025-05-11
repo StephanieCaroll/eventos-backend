ff04ea83d715b10ce1b42679d56a94a39a797b7e
Stephanie <stephaniecaroline2006@gmail.com>
2025-05-11 15:33:24 -0300

    Iniciando o Projeto

---
+++ src/test/java/br/com/ifpe/eventos/EventosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
